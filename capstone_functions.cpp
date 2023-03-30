5db359849fbeacb178fb5dc7e97314ed9cc1ffeb
WV26003 <60018398+WV26003@users.noreply.github.com>
2023-03-30 21:50:10 +0000

    Added the basic header and function file

---
+++ capstone_functions.cpp
@@ -0,0 +1,18 @@
+
+
+/// Header
+#include "capstone_functions.h"
+ 
+
+ //Function Defintions
+void button_toggle(QwiicButton &button, bool &ledState){
+   if (button.hasBeenClicked()) {
+        ledState = !ledState; // Toggle the LED state
+        if (ledState) {
+            button.LEDon(); // Turn the LED on
+        } else {
+            button.LEDoff(); // Turn the LED off
+        }
+        button.clearEventBits(); // Clear button click event
+    }
+}
---
+++ capstone_functions.h
@@ -0,0 +1,22 @@
+#ifndef capstone_functions
+#define capstone_functions
+
+
+// Libraries and Headers
+#include "SparkFun_Qwiic_Button.cpp"
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include "Adafruit_seesaw.h"
+
+//Function prototypes
+
+void button_toggle(QwiicButton &button, bool &ledState); // Qwiic Button On/OFF toggle
+
+
+
+
+
+#endif
