e3028b08146d8ea065438ca61acf5a7fa60fb639
WV26003 <60018398+WV26003@users.noreply.github.com>
2023-03-30 22:23:13 +0000

    Basic Functions added

--- capstone_functions.cpp
+++ capstone_functions.cpp
@@ -16,3 +16,18 @@ void button_toggle(QwiicButton &button, bool &ledState){
         button.clearEventBits(); // Clear button click event
     }
 }
+
+void mute(AudioMixer4 &mixer, int channel_num){
+    mixer.gain(channel_num, 0);
+}
+
+void unmute(AudioMixer4 &mixer, int channel_num){
+    mixer.gain(channel_num, 1);
+}
+
+void linear_fader(Adafruit_seesaw &seesaw, AduioAmplifier &amp, int analog_in){
+    unint16_t analog_read = seesaw.analogRead(analog_in);
+    float fade = analog_read;
+    amp.gain(fade/1023);
+}
+
--- capstone_functions.h
+++ capstone_functions.h
@@ -14,6 +14,9 @@
 //Function prototypes
 
 void button_toggle(QwiicButton &button, bool &ledState); // Qwiic Button On/OFF toggle
+void linear_fader(Adafruit_seesaw &seesaw, AduioAmplifier &amp, unint16_t analog_read);
+void mute(AudioMixer4 &mixer, int channel_num);
+void unmute(AudioMixer4 &mixer, int channel_num);
 
 
 
