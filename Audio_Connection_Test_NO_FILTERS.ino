b4990b6ad0ca3fddd0cba8be600349c2c58a1e5d
Meat-Man01 <129455527+Meat-Man01@users.noreply.github.com>
2023-04-07 02:08:00 -0400

    Add files via upload
    This is functional code incorporating 6 linear faders, 4 encoders/faders, and 8 Qwicc buttons. The last two buttons (red) mute the output mixes. The other 6 buttons (green) do not do anything to the audio channels (They will still alternate between LED on and LED off).

---
+++ Audio_Connection_Test_NO_FILTERS.ino
@@ -0,0 +1,301 @@
+# include <capstone_functions.h>
+
+
+#define ENCODER_SWITCH        24
+#define ENCODER_NEOPIX        6
+
+#define ENCODER_BASE_ADDR          0x36  // I2C address, starts with 0x36
+
+// create 4 encoders
+Adafruit_seesaw encoders[4];
+
+//Stores encoder position
+int32_t encoder_positions[] = {0, 0, 0, 0};
+bool found_encoders[]       = {false, false, false, false};
+
+
+//Setting Addresses for 6 linear faders
+#define  Linear_Fader_ADDR_1 0x30
+#define  Linear_Fader_ADDR_2 0x31
+#define  Linear_Fader_ADDR_3 0x32
+#define  Linear_Fader_ADDR_4 0x33
+#define  Linear_Fader_ADDR_5 0x34
+#define  Linear_Fader_ADDR_6 0x35
+
+#define  L_Fader_ANALOGIN   18
+
+
+Adafruit_seesaw L_Fader_1;
+Adafruit_seesaw L_Fader_2;
+Adafruit_seesaw L_Fader_3;
+Adafruit_seesaw L_Fader_4;
+Adafruit_seesaw L_Fader_5;
+Adafruit_seesaw L_Fader_6;
+
+
+//Quicc Button Setup
+QwiicButton Qbutton1; //0x60
+QwiicButton Qbutton2; //0x61
+QwiicButton Qbutton3; //0x62
+QwiicButton Qbutton4; //0x63
+QwiicButton Qbutton5; //0x64
+QwiicButton Qbutton6; //0x65
+QwiicButton Qbutton7; //0x66
+QwiicButton Qbutton8; //0x67
+uint8_t brightness = 100;     //The brightness to set the LED to when the button is pushed
+bool QLEDState1 = false;      //Can be any value between 0 (off) and 255 (max)
+bool QLEDState2 = false;
+bool QLEDState3 = false;
+bool QLEDState4 = false;
+bool QLEDState5 = false;
+bool QLEDState6 = false;
+bool QLEDState7 = false;
+bool QLEDState8 = false;
+
+
+
+//Audio Signal Flow
+AudioInputI2SQuad        i2s_quad;       //xy=139,685
+AudioAmplifier           amp6;           //xy=334,754
+AudioAmplifier           amp7;           //xy=334,785
+AudioAmplifier           amp8;           //xy=334,815
+AudioAmplifier           amp5;           //xy=335,724
+AudioAmplifier           amp1;           //xy=336,571
+AudioAmplifier           amp2;           //xy=336,601
+AudioAmplifier           amp3;           //xy=336,631
+AudioAmplifier           amp4;           //xy=336,661
+AudioMixer4              mixer1;         //xy=518,615
+AudioMixer4              mixer2;         //xy=518,684
+AudioAmplifier           amp10;          //xy=652,663
+AudioAmplifier           amp9;           //xy=653,632
+AudioOutputI2SQuad       is_quad;        //xy=829,646
+AudioConnection          patchCord1(i2s_quad, 0, amp1, 0);
+AudioConnection          patchCord2(i2s_quad, 0, amp5, 0);
+AudioConnection          patchCord3(i2s_quad, 1, amp2, 0);
+AudioConnection          patchCord4(i2s_quad, 1, amp6, 0);
+AudioConnection          patchCord5(i2s_quad, 2, amp3, 0);
+AudioConnection          patchCord6(i2s_quad, 2, amp7, 0);
+AudioConnection          patchCord7(i2s_quad, 3, amp4, 0);
+AudioConnection          patchCord8(i2s_quad, 3, amp8, 0);
+AudioConnection          patchCord9(amp6, 0, mixer2, 1);
+AudioConnection          patchCord10(amp7, 0, mixer2, 2);
+AudioConnection          patchCord11(amp8, 0, mixer2, 3);
+AudioConnection          patchCord12(amp5, 0, mixer2, 0);
+AudioConnection          patchCord13(amp1, 0, mixer1, 0);
+AudioConnection          patchCord14(amp2, 0, mixer1, 1);
+AudioConnection          patchCord15(amp3, 0, mixer1, 2);
+AudioConnection          patchCord16(amp4, 0, mixer1, 3);
+AudioConnection          patchCord17(mixer1, amp9);
+AudioConnection          patchCord18(mixer2, amp10);
+AudioConnection          patchCord19(amp10, 0, is_quad, 2);
+AudioConnection          patchCord20(amp10, 0, is_quad, 3);
+AudioConnection          patchCord21(amp9, 0, is_quad, 0);
+AudioConnection          patchCord22(amp9, 0, is_quad, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=356.5,338
+AudioControlSGTL5000     sgtl5000_2;     //xy=511.5,339
+
+
+
+void setup() {
+  AudioMemory(12);
+
+  // Enable the first audio shield, select input, and enable output
+  sgtl5000_1.setAddress(LOW);
+  sgtl5000_1.enable();
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
+  sgtl5000_1.volume(0.5);
+  sgtl5000_1.lineOutLevel(29);
+
+  // Enable the second audio shield, select input, and enable output
+  sgtl5000_2.setAddress(HIGH);
+  sgtl5000_2.enable();
+  sgtl5000_2.inputSelect(AUDIO_INPUT_LINEIN);
+  sgtl5000_2.volume(0.5);
+  sgtl5000_2.lineOutLevel(29);
+
+
+//Engaging Encoders
+Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  Serial.println("Looking for seesaws!");
+
+  for (uint8_t enc=0; enc<sizeof(found_encoders); enc++) {
+    // See if we can find encoders on this address
+    if (! encoders[enc].begin(ENCODER_BASE_ADDR + enc)) {
+      Serial.print("Couldn't find encoder #");
+      Serial.println(enc);
+    } 
+    else {
+      Serial.print("Found encoder");
+      Serial.println(enc);
+
+      uint32_t version = ((encoders[enc].getVersion() >> 16) & 0xFFFF);
+      if (version != 4991){
+        Serial.print("Wrong firmware loaded? ");
+        Serial.println(version);
+        while(1) delay(10);
+      }
+
+      // use a pin for the built in encoder switch
+      encoders[enc].pinMode(ENCODER_SWITCH, INPUT_PULLUP);
+
+      // get starting position
+      encoder_positions[enc] = encoders[enc].getEncoderPosition();
+    }
+  }
+
+//Engaging Linear Faders
+  if (!L_Fader_1.begin(Linear_Fader_ADDR_1)){
+    Serial.println(F("L_Fader_1 not found!"));
+    while(1) delay(10);
+  }
+  else {
+    Serial.println(F("L_Fader_1 found!"));
+  }
+  
+  if (!L_Fader_2.begin(Linear_Fader_ADDR_2)){
+    Serial.println(F("L_Fader_2 not found!"));
+    while(1) delay(10);
+  }
+  else {
+    Serial.println(F("L_Fader_2 found!"));
+  }
+  
+  if (!L_Fader_3.begin(Linear_Fader_ADDR_3)){
+    Serial.println(F("L_Fader_3 not found!"));
+    while(1) delay(10);
+  }
+  else {
+    Serial.println(F("L_Fader_3 found!"));
+  }
+  
+  if (!L_Fader_4.begin(Linear_Fader_ADDR_4)){
+    Serial.println(F("L_Fader_4 not found!"));
+    while(1) delay(10);
+  }
+    else {
+    Serial.println(F("L_Fader_4 found!"));
+  }
+
+  if (!L_Fader_5.begin(Linear_Fader_ADDR_5)){
+    Serial.println(F("L_Fader_5 not found!"));
+    while(1) delay(10);
+  }
+  else {
+    Serial.println(F("L_Fader_5 found!"));
+  }
+  
+  if (!L_Fader_6.begin(Linear_Fader_ADDR_6)){
+    Serial.println(F("L_Fader_6 not found!"));
+    while(1) delay(10);
+  }
+  else {
+    Serial.println(F("L_Fader_6 found!"));
+  }
+
+
+//Engaging Buttons
+  Wire2.begin();
+  if (Qbutton1.begin(0x60, Wire2) == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+  Serial.println("Qbutton1 acknowledged.");
+  if (Qbutton2.begin(0x61, Wire2) == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+  
+  Serial.println("Qbutton2 acknowledged.");
+  if (Qbutton3.begin(0x62, Wire2) == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+  Serial.println("Qbutton3 acknowledged.");
+ 
+  if (Qbutton4.begin(0x63, Wire2) == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+
+  Serial.println("Qbutton4 acknowledged.");
+  if (Qbutton5.begin(0x64, Wire2) == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+  Serial.println("Qbutton5 acknowledged.");
+  if (Qbutton6.begin(0x65, Wire2) == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+    Serial.println("Qbutton6 acknowledged.");
+     if (Qbutton7.begin(0x66, Wire2) == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+    Serial.println("Qbutton7 acknowledged.");
+     if (Qbutton8.begin(0x67, Wire2) == false) {
+    Serial.println("Device did not acknowledge! Freezing.");
+    while (1);
+  }
+    Serial.println("Qbutton8 acknowledged.");
+
+    
+//Start LEDs off
+  Qbutton1.LEDoff();
+  Qbutton2.LEDoff();
+  Qbutton3.LEDoff();
+  Qbutton4.LEDoff();
+  Qbutton5.LEDoff();
+  Qbutton6.LEDoff();
+  Qbutton7.LEDoff();
+  Qbutton8.LEDoff();
+
+
+  
+}
+
+
+void loop() {
+
+//Linear Fader Control of Primary Mix Inputs
+  linear_fader(L_Fader_1, amp1, L_Fader_ANALOGIN);
+  linear_fader(L_Fader_2, amp2, L_Fader_ANALOGIN);
+  linear_fader(L_Fader_3, amp3, L_Fader_ANALOGIN);
+  linear_fader(L_Fader_4, amp4, L_Fader_ANALOGIN);
+
+//Pre-amp gain of the four inputs defined by first four encoders
+  encoder_fader(encoders[0], amp5, encoder_positions[0]);
+  encoder_fader(encoders[1], amp6, encoder_positions[1]);
+  encoder_fader(encoders[2], amp7, encoder_positions[2]);
+  encoder_fader(encoders[3], amp8, encoder_positions[3]);
+
+//Linear Fader Control of Output Mixes (Primary and Auxiliary)
+  linear_fader(L_Fader_5, amp9, L_Fader_ANALOGIN);
+  linear_fader(L_Fader_6, amp10, L_Fader_ANALOGIN);
+  //Serial.println("");
+
+//Toggling Qwicc Buttons
+  button_toggle(Qbutton1, QLEDState1);
+  button_toggle(Qbutton2, QLEDState2);
+  button_toggle(Qbutton3, QLEDState3);
+  button_toggle(Qbutton4, QLEDState4);
+  button_toggle(Qbutton5, QLEDState5);
+  button_toggle(Qbutton6, QLEDState6);
+  button_toggle(Qbutton7, QLEDState7);
+  button_toggle(Qbutton8, QLEDState8);
+
+//Muting Output Mixes Via Qwicc Buttons
+
+  output_mix_mute_control(mixer1, 0, QLEDState7);
+  output_mix_mute_control(mixer1, 1, QLEDState7);
+  output_mix_mute_control(mixer1, 2, QLEDState7);
+  output_mix_mute_control(mixer1, 3, QLEDState7);
+
+  output_mix_mute_control(mixer2, 0, QLEDState8);
+  output_mix_mute_control(mixer2, 1, QLEDState8);
+  output_mix_mute_control(mixer2, 2, QLEDState8);
+  output_mix_mute_control(mixer2, 3, QLEDState8);
+
+}
